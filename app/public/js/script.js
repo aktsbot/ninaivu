bb42aab239bea99db274534091f42ecf3594552c
aktsbot <box.ashishk@gmail.com>
2024-03-13 07:25:51 +0530

    add confirm for delete message

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -1,1 +1,16 @@
-// if you want, add global js code here!
+// https://stackoverflow.com/questions/10462839/how-to-display-a-confirmation-dialog-when-clicking-an-a-link
+function hookConfirmDelete() {
+  var elems = document.getElementsByClassName("confirm-delete");
+  var confirmIt = function (e) {
+    if (!confirm("Are you sure?")) e.preventDefault();
+  };
+  for (var i = 0, l = elems.length; i < l; i++) {
+    elems[i].addEventListener("click", confirmIt, false);
+  }
+}
+
+function start() {
+  hookConfirmDelete();
+}
+
+start();
