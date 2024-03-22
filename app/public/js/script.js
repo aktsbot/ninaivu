901e481dd3c387dc7e064a4f86a1322831dc2fba
aktsbot <box.ashishk@gmail.com>
2024-03-22 08:11:34 +0530

    fix date time string in report

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -1,7 +1,19 @@
+function formatDate(value) {
+  let date = new Date(value);
+  const day = date.toLocaleString("default", { day: "2-digit" });
+  const month = date.toLocaleString("default", { month: "short" });
+  const year = date.toLocaleString("default", { year: "numeric" });
+  const time = date.toLocaleString("default", {
+    hour: "numeric",
+    minute: "numeric",
+  });
+  return day + "/" + month + "/" + year + " " + time;
+}
+
 function fixDateTime() {
   document.querySelectorAll(".fix-datetime").forEach((e) => {
     let text = e.textContent;
-    const date = new Date(text).toLocaleString("en-IN");
+    const date = formatDate(text);
     e.textContent = date;
   });
 }
